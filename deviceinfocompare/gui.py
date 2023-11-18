71e63c515e4d4eefeb49fc5c00edae70bbd9ab8c
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2023-11-18 13:50:16 +0300

    #patch made devicelist visible

--- deviceinfocompare/gui.py
+++ deviceinfocompare/gui.py
@@ -1,15 +1,25 @@
-import glob
+import datetime
 import logging
 import os
+import platform
+import re
 import sys
+from typing import Union
 
 from PyQt6 import QtGui, QtWidgets, uic
+from PyQt6.QtCore import QStringListModel
 from PyQt6.QtWidgets import QMessageBox
 
 import deviceinfocompare
+from deviceinfocompare.processors import *
 from deviceinfocompare.settings import LOGGING, RESOURCE_PATH
 
 logger = logging.getLogger(__name__)
+
+
+def strip_s_ms(dt_in: Union[str, datetime.datetime]):
+    dt_in = str(dt_in)
+    return re.sub(r":\d+\.\d+$", "", dt_in)
 
 
 class QTextEditLogger(logging.Handler):
@@ -34,10 +44,20 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__(*args, **kwargs)
         uic.loadUi(os.path.join(RESOURCE_PATH, "ui", "deviceinfocompare.ui"), self)
 
+        system_name = platform.system()
+        data_processor = None
+
+        if system_name == "Windows":
+            data_processor = WindowsProcessor()
+        else:
+            raise Exception(f"DIC can't work on {system_name} yet")
+
+        # region Configuring logger
         logTextBox = QTextEditLogger(self.loggingTextEdit)
         logging.getLogger().addHandler(logTextBox)
 
         logger.info("The program has started")
+        # endregion
 
         self.centralWidget.setContentsMargins(11, 11, 11, 11)
         self.setWindowIcon(
@@ -55,6 +75,18 @@ class MainWindow(QtWidgets.QMainWindow):
         # endregion
 
         self.connectEvents()
+
+        dump_data_list = data_processor.get_dump_list()
+        entries = (
+            f"#0 [{strip_s_ms(datetime.datetime.utcnow())}] Current device list",
+            *[f"#{i[0]} [{strip_s_ms(i[1])}] {i[2]}" for i in dump_data_list],
+        )
+
+        model = QStringListModel()
+        model.setStringList(entries)
+
+        self.leftListView.setModel(model)
+        self.rightListView.setModel(model)
 
 
 app = QtWidgets.QApplication(sys.argv)
